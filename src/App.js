2dfc7676302ad59e51666aa1c3a064d37c67691f
rushankhur <kazanface@gmail.com>
2020-09-09 14:57:52 -0300

    pricing completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
-import Featured from './components/featured/index';
+import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
+import Highlights from './components/highlights';
+import Pricing from './components/pricing';
 
 function App() {
   return (
-    <div className="App" style={{ height:"1500px", background: 'cornflowerblue'}}>
+    <div className="App" style={{ height:"1500px" }}>
         <Header />
         <Featured />
+        <VenueNfo />
+        <Highlights />
+        <Pricing />
     </div>
   );
 }
---
+++ src/components/highlights/Description.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+                <div className="highlight_description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada nibh id erat mollis blandit. 
+                Duis neque dui, lobortis vel nunc vitae, fermentum volutpat dui. Proin gravida, sem in rhoncus pretium,
+                enim libero tempus augue, a pulvinar purus libero ut mauris. Cras molestie eros vulputate, dapibus nisi
+                quis, sollicitudin sem. Etiam pellentesque laoreet sem.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
